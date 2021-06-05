7bac42f324df379a223d78419763720a1279d74b
manas9927 <manasvjbp27@gmail.com>
2021-06-05 20:28:01 +0530

    :bug: Fix server error

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 const app = express();
-const port = 5000;
+const port = process.env.PORT || 5000;
 
 var MongoClient = require('mongodb').MongoClient
 
